d7d6d7a36a8d2f99fba5b6cb86641cb4dcdf9745
Neill Robson <neill@neillrobson.com>
2019-05-10 10:27:34 -0400

    Use gulp for build.js functionality

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,9 +1,25 @@
 import BS from 'browser-sync';
 import cp from 'child_process';
+import flatten from 'gulp-flatten';
 import fs from 'fs-extra';
 import gulp from 'gulp';
+import path from 'path';
 import sass from 'gulp-sass';
 import node_sass from 'node-sass';
+import node_sass_tilde_importer from 'node-sass-tilde-importer';
+
+const pluginFiles = {
+    'jquery/dist/jquery.min.js': '.',
+    'popper.js/dist/umd/popper.min.js': '.',
+    'bootstrap/dist/js/*': 'bootstrap/js',
+    'flickity/dist/*.min.*': 'flickity',
+    '@fortawesome/fontawesome-free/js/all.min.js': 'fontawesome',
+    'imagesloaded/imagesloaded.pkgd.min.js': '.',
+    'isotope-layout/dist/isotope.pkgd.min.js': '.',
+    'highlightjs/highlight.pack.min.js': 'highlight',
+    'highlightjs/styles/*': 'highlight/styles',
+    'jquery-validation/dist/jquery.validate.min.js': 'jquery-validation'
+};
 
 const browsersync = BS.create();
 
@@ -25,23 +41,41 @@ function bsReload(done) {
 }
 
 function clean() {
-    return fs.emptyDir('./_site');
+    return Promise.all([
+        fs.emptyDir('./_site'),
+        fs.emptyDir('./_sass/bootstrap')
+    ]);
 }
 
-function css() {
+function compileSass() {
     return gulp
         .src("./_sass/*.scss")
-        .pipe(sass())
+        .pipe(sass({
+            importer: node_sass_tilde_importer
+        }))
         .pipe(gulp.dest("./_site/assets/css"))
         .pipe(browsersync.stream());
 }
 
+function copyJs(src, dest) {
+    return gulp
+        .src(path.join('./node_modules', src))
+        .pipe(flatten())
+        .pipe(gulp.dest(path.join('./_site/assets/plugins', dest)))
+        .pipe(browsersync.stream());
+}
+
+const loadPluginJs = gulp.parallel.apply(
+    null, Object.entries(pluginFiles).map(e => copyJs.bind(null, ...e)));
+
 function jekyll() {
-    return cp.spawn("bundle.bat", ["exec", "jekyll", "build"], { stdio: "inherit" });
+    return cp.spawn(
+        "bundle.bat", ["exec", "jekyll", "build"], { stdio: "inherit" });
 }
 
 function watchFiles() {
-    gulp.watch("./_sass/**/*", css);
+    gulp.watch("./_sass/**/*", compileSass);
+    gulp.watch( "./node_modules/**/*", loadPluginJs);
     gulp.watch([
         "./_includes/**/*",
         "./_pages/**/*",
@@ -52,7 +86,7 @@ function watchFiles() {
     ], gulp.series(jekyll, bsReload));
 }
 
-const build = gulp.series(clean, gulp.parallel(css, jekyll));
+const build = gulp.series(clean, gulp.parallel(compileSass, loadPluginJs), jekyll);
 const watch = gulp.parallel(bsInit, watchFiles);
 
 export {
