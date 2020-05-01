b949d04cd825f07c406e6a7b1888b98fb641f173
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-01 01:22:51 +0000

    Deploy neillrobson/neillrobson.github.io to github.com/neillrobson/neillrobson.github.io.git:master

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	
+	/* ======= Sliding menu underline ====== */
+	// Menu has active item
+	//Ref: https://codepen.io/digistate/pen/OXXjXM
+	 
+	debugger;
+	$nav = $("#navigation"),
+	$slideLine = $("#slide-line"),
+	$currentItem = $("#navigation li.active");
+
+	function refresh() {
+		if ($currentItem[0]) {
+			$slideLine.css({
+			  "width": $currentItem.width(),
+			  "left": $currentItem.position().left
+			});
+		} else {
+			// Disapear
+			$slideLine.width(0);
+		}
+	}
+
+	refresh();
+
+	$(window).on('resize load', refresh);
+	
+	// Underline transition
+	$nav.find("li").hover(
+		// Hover on
+		function(){
+		  $slideLine.css({
+		    "width": $(this).width(),
+		    "left": $(this).position().left
+		  });
+		},
+		// Hover out
+		refresh
+	);
+    
+
+});
