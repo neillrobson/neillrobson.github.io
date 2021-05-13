4e9a4790391a6f3460607827014fb2ab893fd430
Neill Robson <neill@neillrobson.com>
2021-05-13 11:03:58 -0400

    Convert style to SCSS

--- webpack/entry.js
+++ webpack/entry.js
@@ -2,7 +2,7 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedinIn, faGithubAlt, faTwitter, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
-import './style.css';
+import './style.scss';
 import './social.scss';
 import './autumn.css';
 
